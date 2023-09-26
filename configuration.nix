1264eda9ab6e4a7e14e20ad5dde3036998c2da68
micronoyau <leo.lf.dk@gmail.com>
2023-09-26 12:24:53 +0200

    Cleaned up form

--- configuration.nix
+++ configuration.nix
@@ -17,26 +17,9 @@ boot.loader.systemd-boot.enable = true;
 boot.loader.efi.canTouchEfiVariables = true;
 
 networking.hostName = "pwnixos"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 # Enable networking
 networking.networkmanager.enable = true;
-
-# Fonts
-fonts.fonts = with pkgs; [
-  (nerdfonts.override { fonts = [ "SourceCodePro"  ]; })
-];
-
-#console = {
-#  earlySetup = true;
-#  font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
-#  packages = with pkgs; [ terminus_font ];
-#  keyMap = "us";
-#};
 
 # Set your time zone.
 time.timeZone = "Europe/Paris";
@@ -81,16 +64,12 @@ services.pipewire = {
   alsa.enable = true;
   alsa.support32Bit = true;
   pulse.enable = true;
-  # If you want to use JACK applications, uncomment this
-  #jack.enable = true;
-
-  # use the example session manager (no others are packaged yet so this is enabled by default,
-  # no need to redefine it in your config for now)
-  #media-session.enable = true;
 };
 
-# Enable touchpad support (enabled default in most desktopManager).
-# services.xserver.libinput.enable = true;
+# Fonts
+fonts.fonts = with pkgs; [
+  (nerdfonts.override { fonts = [ "SourceCodePro"  ]; })
+];
 
 # Sudo
 security.sudo = {
@@ -121,106 +100,93 @@ virtualisation.docker.enable = true;
 virtualisation.virtualbox.host.enable = true;
 users.extraGroups.vboxusers.members = [ "micronoyau" ];
 
+# Allow unfree packages
+nixpkgs.config.allowUnfree = true;
+
+nix.settings.extra-experimental-features = "nix-command flakes";
+
 # Define a user account. Don't forget to set a password with ‘passwd’.
 users.users.micronoyau = {
   isNormalUser = true;
   description = "micronoyau";
   extraGroups = [ "networkmanager" "wheel" "wireshark" "docker" ];
-    packages = with pkgs; [
-      firefox
-      docker
-      docker-compose
-      zotero
-      qemu
-      neovim
-      ripgrep
-      tmux
-      anki
-      nodejs_20
-      coq
-      coqPackages.coqide
-      poppler_utils
-      exiftool
-      spotify
-      # Security
-      wireshark
-      nmap
-      ghidra
-      burpsuite
-      dirb
-      gobuster
-      ffuf
-      dnsrecon
-      dig
-      radare2
-      (import ./sublist3r.nix)
-    ];
-  };
+  packages = with pkgs; [
+    firefox
+    docker
+    docker-compose
+    zotero
+    qemu
+    neovim
+    ripgrep
+    tmux
+    anki
+    nodejs_20
+    coq
+    coqPackages.coqide
+    poppler_utils
+    exiftool
+    spotify
+    # Security
+    wireshark
+    nmap
+    ghidra
+    burpsuite
+    dirb
+    gobuster
+    ffuf
+    dnsrecon
+    dig
+    radare2
+    (import ./sublist3r.nix)
+  ];
+};
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    tree
-    htop
-    file
-    gcc_multi
-    gdb
-    python311
-    python311Packages.virtualenv
-    python311Packages.pip
-    keepassxc
-    pciutils
-    wirelesstools
-    patchelf
-    traceroute
-    jdk17
-    unzip
-    ascii
-    findutils
-    mlocate
-    glibc
-    gnupg
-    pinentry
-    glibc_multi
-    git
-    xclip
-    openvpn
-    inetutils
-    sage
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+environment.systemPackages = with pkgs; [
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  tree
+  htop
+  file
+  gcc_multi
+  gdb
+  python311
+  python311Packages.virtualenv
+  python311Packages.pip
+  keepassxc
+  pciutils
+  wirelesstools
+  patchelf
+  traceroute
+  jdk17
+  unzip
+  ascii
+  findutils
+  mlocate
+  glibc
+  gnupg
+  pinentry
+  glibc_multi
+  git
+  xclip
+  openvpn
+  inetutils
+  sage
 ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.wireshark.enable = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+programs.wireshark.enable = true;
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = true;
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+system.stateVersion = "23.05"; # Did you read the comment?
+system.autoUpgrade.enable = true;
+system.autoUpgrade.allowReboot = true;
 }
+
